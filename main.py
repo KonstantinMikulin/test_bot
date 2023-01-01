e6bbf5fe7e1a7fee9c81c7f1d4242cc339fa7b9c
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-01-01 18:56:09 +0300

    Send back only two words and upper

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ bot = Bot(config.TOKEN)
 dp = Dispatcher(bot)
 
 
+# @dp.message_handler()
+# async def echo (message: types.Message):
+#     await message.answer(text=message.text.upper())
+
 @dp.message_handler()
 async def echo (message: types.Message):
-    await message.answer(text=message.text)
+    if message.text.count(' ') == 1:
+        await message.answer(text=message.text)
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
