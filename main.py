aeee11ace8788e05cb5b7a0c5f50fe9da24f86c6
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-01-01 18:40:44 +0300

    one more

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-import aiogram
+from aiogram import Bot, Dispatcher, executor, types
+import config
+
+
+bot = Bot(config.TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo (message: types.Message):
+    await message.answer(text=message.text)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
