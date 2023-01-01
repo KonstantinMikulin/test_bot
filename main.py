84247475001ce7023ff361048a8bcae72409e79e
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-01-01 19:55:15 +0300

    add start and help commans

--- main.py
+++ main.py
@@ -1,19 +1,36 @@
 from aiogram import Bot, Dispatcher, executor, types
 import config
 
-
 bot = Bot(config.TOKEN)
 dp = Dispatcher(bot)
 
+HELP_COMMAND = '''
+/help - список команд
+/start - начало работы
+'''
 
-# @dp.message_handler()
-# async def echo (message: types.Message):
-#     await message.answer(text=message.text.upper())
+
+@dp.message_handler(commands=['start', 'старт'])
+async def help_command(message: types.Message):
+    await message.answer(text='Welcome')
+    await message.delete()
+
+
+@dp.message_handler(commands=['help', 'помощь'])
+async def help_command(message: types.Message):
+    await message.reply(text=HELP_COMMAND)
+    await message.delete()
+
 
 @dp.message_handler()
-async def echo (message: types.Message):
-    if message.text.count(' ') == 1:
-        await message.answer(text=message.text)
+async def echo(message: types.Message):
+    await message.answer(text=message.text.upper())
+
+
+# @dp.message_handler()
+# async def echo(message: types.Message):
+#     if message.text.count(' ') == 1:
+#         await message.answer(text=message.text)
 
 
 if __name__ == '__main__':
