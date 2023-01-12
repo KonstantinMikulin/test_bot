ca548b6efe572ce64af9d3e6d9bbc12b9f6e6655
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-01-12 23:30:19 +0300

    commit

--- main_temp.py
+++ main_temp.py
@@ -1,41 +1,17 @@
 from aiogram import Bot, Dispatcher, executor, types
 import config
 
-bot = Bot(config.TOKEN)
+bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
-HELP_COMMAND = '''
-<b>/start</b> - <em>Run the Bot!</em>
-<b>/help</b> - <em>Got help?</em>
+HELP_COMMANDS = '''
+<b>/help</b> <em>- show some help!</em>
 '''
-
-
 @dp.message_handler(commands=['help'])
 async def help_command(message: types.Message):
-    await bot.send_message(chat_id=message.from_user.id,
-                           text=HELP_COMMAND,
+    await bot.send_message(message.from_user.id,
+                           text=HELP_COMMANDS,
                            parse_mode='HTML')
-    await message.delete()
-
-
-@dp.message_handler(commands=['photo'])
-async def send_image(message: types.Message):
-    await bot.send_photo(chat_id=message.chat.id,
-                         photo='https://qph.cf2.quoracdn.net/main-qimg-1947fbba660f801fbb2261c4e6d7a336-lq')
-
-
-@dp.message_handler(commands='location')
-async def send_coord(message: types.Message):
-    await bot.send_location(chat_id=message.from_user.id,
-                            latitude=55,
-                            longitude=50)
-    await message.delete()
-
-
-@dp.message_handler()
-async def echo(message: types.Message):
-    await bot.send_message(chat_id=message.chat.id, text='Hello')
-
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
