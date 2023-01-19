1bc128143314708dd7d6fecbbeae62ac4dfe290b
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-01-20 00:04:09 +0300

    commit

--- main_temp.py
+++ main_temp.py
@@ -11,8 +11,9 @@ keyboard = ReplyKeyboardMarkup(resize_keyboard=True,
 button1 = KeyboardButton('/help')
 button2 = KeyboardButton('/description')
 button3 = KeyboardButton('❤️')
+button4 = KeyboardButton('/sendorange')
 
-keyboard.add(button1).add(button2).add(button3)
+keyboard.add(button1).add(button2).add(button3).add(button4)
 
 HELP_COMMANDS = '''
 <b>/start</b> - Run The Bot!
@@ -56,6 +57,12 @@ async def send_love_command(message: types.Message):
 async def send_sticker_command(message: types.Message):
     await bot.send_sticker(chat_id=message.chat.id,
                            sticker='CAACAgUAAxkBAAEHHGBjtEA8BvZS2jNkA-jZn8jXxCAijQAChQMAAukKyANnDaiEsg4V0S0E')
+
+
+@dp.message_handler(commands='sendorange')
+async def send_orange(message: types.Message):
+    await bot.send_photo(chat_id=message.chat.id,
+                         photo='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFdUlevaWPqu7lObc3QkkGCZEbkRaN_yA-bw&usqp=CAU')
 
 
 
