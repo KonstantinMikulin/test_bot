67cd6cccfacb543c13ab8a30ef43932cd25f59bd
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-01-20 19:45:33 +0300

    commit

--- config.py
+++ config.py
@@ -1,1 +1,23 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
 TOKEN ='5751024125:AAFHkyBrgab3LVI_OpmmpHoyhw-ts2Dqlo4'
+
+
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True,
+                               one_time_keyboard=True)
+button1 = KeyboardButton('/start')
+button2 = KeyboardButton('/links')
+keyboard.add(button1, button2)
+
+inline_keyboard = InlineKeyboardMarkup(row_width=3)
+inline_button1 = InlineKeyboardButton(text='link 1',
+                                      url='https://ru.wikipedia.org/')
+inline_button2 = InlineKeyboardButton(text='link 2',
+                                      url='https://www.youtube.com/')
+inline_button3 = InlineKeyboardButton(text='link 3',
+                                      url='https://vk.com/')
+inline_button4 = InlineKeyboardButton(text='link 4',
+                                      url='https://github.com/')
+inline_keyboard.add(inline_button1, inline_button2, inline_button3, inline_button4)
--- main_temp.py
+++ main_temp.py
@@ -1,6 +1,4 @@
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 import config
 
@@ -12,29 +10,11 @@ async def on_startup(_):
     print('Bot is running!')
 
 
-keyboard = ReplyKeyboardMarkup(resize_keyboard=True,
-                               one_time_keyboard=True)
-button1 = KeyboardButton('/start')
-button2 = KeyboardButton('/links')
-keyboard.add(button1, button2)
-
-inline_keyboard = InlineKeyboardMarkup(row_width=3)
-inline_button1 = InlineKeyboardButton(text='link 1',
-                                      url='https://ru.wikipedia.org/')
-inline_button2 = InlineKeyboardButton(text='link 2',
-                                      url='https://www.youtube.com/')
-inline_button3 = InlineKeyboardButton(text='link 3',
-                                      url='https://vk.com/')
-inline_button4 = InlineKeyboardButton(text='link 4',
-                                      url='https://github.com/')
-inline_keyboard.add(inline_button1, inline_button2, inline_button3, inline_button4)
-
-
 @dp.message_handler(commands='start')
 async def start_command(message: types.Message):
     await bot.send_message(chat_id=message.chat.id,
                            text='Welcome',
-                           reply_markup=keyboard)
+                           reply_markup=config.keyboard)
     await message.delete()
 
 
@@ -42,7 +22,7 @@ async def start_command(message: types.Message):
 async def links_command(message: types.Message):
     await bot.send_message(chat_id=message.chat.id,
                            text='Here you are',
-                           reply_markup=inline_keyboard)
+                           reply_markup=config.inline_keyboard)
     await message.delete()
 
 
