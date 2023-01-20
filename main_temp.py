8e9b2423b9dab5f0dd86b8bcdeebc23bcf80087c
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-01-20 18:13:59 +0300

    commit

--- main_temp.py
+++ main_temp.py
@@ -6,15 +6,30 @@ import config
 bot = Bot(config.TOKEN)
 dp = Dispatcher(bot)
 
-button1 = InlineKeyboardButton('start')
-button2 = InlineKeyboardButton('help')
+inline_keyboard = InlineKeyboardMarkup(row_width=2)
+inline_button1 = InlineKeyboardButton(text='WiKi',
+                                  url='https://ru.wikipedia.org/')
+inline_button2 = InlineKeyboardButton(text='YouTube',
+                                  url='https://www.youtube.com/')
+
+inline_keyboard.add(inline_button1, inline_button2)
+
+
 
 
 @dp.message_handler(commands='start')
 async def start_command(message: types.Message):
     await bot.send_message(chat_id=message.from_user.id,
-                           reply_markup=InlineKeyboardMarkup)
+                           text='Welcome!',
+                           reply_markup=inline_keyboard)
+
+
+@dp.message_handler(commands='help')
+async def help_command(message: types.Message):
+    await bot.send_message(chat_id=message.chat.id,
+                           text='Need help?')
+
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
