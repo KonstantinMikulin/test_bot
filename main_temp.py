769108a299d5829cf1c6f3256f9e328c510dd8bf
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-02-02 22:54:25 +0300

    commit

--- main_temp.py
+++ main_temp.py
@@ -1,21 +1,22 @@
 import asyncio
 
-async def send_hello():
-    await asyncio.sleep(2)
-    print('Hello!')
 
-
-async def send_bye():
-    await asyncio.sleep(1)
-    print('Bye!')
+async def send_one():
+    n = 0
+    while True:
+        await asyncio.sleep(1)
+        n += 1
+        if n % 3 != 0:
+            print(f'Прошло {n} секунд')
+        elif n % 3 == 0:
+            print('Прошло еще три секунды')
 
 
 async def main():
-    task_1 = asyncio.create_task(send_hello())
-    task_2 = asyncio.create_task(send_bye())
+    task_1 = asyncio.create_task(send_one())
 
     await task_1
-    await task_2
 
 
-asyncio.run(main())
+if __name__ == '__main__':
+    asyncio.run(main())
