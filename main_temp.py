858a416922f86d370b11039844eacddd32e62d37
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-02-02 23:33:50 +0300

    commit

--- main_temp.py
+++ main_temp.py
@@ -1,22 +1,31 @@
-import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+import config
+
+bot = Bot(config.TOKEN)
+dp = Dispatcher(bot)
 
 
-async def send_one():
-    n = 0
-    while True:
-        await asyncio.sleep(1)
-        n += 1
-        if n % 3 != 0:
-            print(f'Прошло {n} секунд')
-        elif n % 3 == 0:
-            print('Прошло еще три секунды')
+async def on_startup(_):
+    print('Bot is running')
 
 
-async def main():
-    task_1 = asyncio.create_task(send_one())
+@dp.message_handler(commands='start')
+async def cmd_start(message: types.Message):
+    await bot.send_photo(chat_id=message.chat.id,
+                         caption='Like it?',
+                         photo='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMzmn84CZqMM3f1jK009KsRRf_W8q5vF0q-g&usqp=CAU',
+                         reply_markup=config.inline_keyboard)
 
-    await task_1
+
+@dp.callback_query_handler()
+async def ikb_callback_handler(callback: types.CallbackQuery):
+    if callback.data == 'like':
+        await callback.answer(text='You like it!')
+    else:
+        await callback.answer(text='You don`t like it')
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    executor.start_polling(dp,
+                           on_startup=on_startup,
+                           skip_updates=True)
