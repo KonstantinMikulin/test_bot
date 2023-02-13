577884236bc89cc1975cb08928f3f818506bc60e
Konstantin Mikulin <mikulin.konstantin@gmail.com>
2023-02-13 18:05:29 +0300

    commit

--- main_temp.py
+++ main_temp.py
@@ -7,22 +7,28 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-class TestMiddleWare(BaseMiddleware):
-
-    async def on_process_update(self, update, data):
-        print('Process update done')
+class CustomMiddleware(BaseMiddleware):
 
     async def on_pre_process_update(self, update: types.Update, data: dict):
-        print('Hello!')
+        pass
+
+    async def on_process_update(self, update: types.Update, data: dict):
+        pass
+
+    async def on_process_message(self, message: types.Message, data: dict):
+        print(data, message)
 
 
 @dp.message_handler(commands='start')
 async def cmd_start(message: types.Message):
-    await message.answer(text='Started')
-    print('World!')
+    ikb = types.InlineKeyboardMarkup(inline_keyboard=[
+        [types.InlineKeyboardButton('Test', callback_data='data')]
+    ])
+    await message.answer(text='Started',
+                         reply_markup=ikb)
 
 
 if __name__ == '__main__':
-    dp.middleware.setup(TestMiddleWare())
+    dp.middleware.setup(CustomMiddleware())
     executor.start_polling(dispatcher=dp,
                            skip_updates=True)
