8d175923bb86af9836461be20676cded0cf09236
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-10-30 20:13:29 +0300

    Merge branch 'main_menu' into branch 'main'

--- bot/handlers/main_menu/commands_list.py
+++ bot/handlers/main_menu/commands_list.py
@@ -3,6 +3,6 @@ from aiogram.types import BotCommand
 # list of commands for main menu button
 main_menu_commands = [
     BotCommand(command='/start', description='Start bot'),
-    BotCommand(command='/check', description='Command for user'),
-    BotCommand(command='/test', description='Command for admin')
+    BotCommand(command='/check', description='Cmd for user'),
+    BotCommand(command='/test', description='Cmd for admin')
 ]
