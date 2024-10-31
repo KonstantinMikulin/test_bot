a7e0807e41b9274e223ea9a177f31deba1e7d4dd
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-10-31 14:22:12 +0300

    Add simple url keyboard and handler for its command /diet

--- bot/handlers/user_commands.py
+++ bot/handlers/user_commands.py
@@ -2,6 +2,8 @@ from aiogram import Router
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
+from bot.keyboards import url_keyboard
+
 # creating router`s onject
 router = Router(name='user router')
 
@@ -16,3 +18,12 @@ async def cmd_start(message: Message):
 @router.message(Command(commands='weight'))
 async def cmd_check(message: Message):
     await message.answer('Your record was add to database, user!')
+
+
+# command for /diet for everyone
+@router.message(Command(commands='diet'))
+async def cmd_diet(message: Message):
+    await message.answer(
+        text='Heare your food and goal',
+        reply_markup=url_keyboard
+    )
