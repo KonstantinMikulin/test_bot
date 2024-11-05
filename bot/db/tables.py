fd72d2a9f853469acb199b91117982c2d1fbd7bc
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-11-05 16:56:14 +0300

    Add db package, add model for users table

---
+++ bot/db/tables.py
@@ -0,0 +1,11 @@
+from sqlalchemy import MetaData, Table, Column, BigInteger, String
+
+metadata = MetaData()
+
+users = Table(
+    "users",
+    metadata,
+    Column("telegram_id", BigInteger, primary_key=True),
+    Column("first_name", String),
+    Column("last_name", String)
+)
