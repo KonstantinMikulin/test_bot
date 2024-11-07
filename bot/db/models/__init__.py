cc3c67eeb2d97ce30f2a073e64e5c208c8680a20
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-11-07 19:59:51 +0300

    Add new model for table Measurments

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -1,7 +1,5 @@
 from .user import User
 from .user_weights import Weight
+from .user_measurement import Measurement
 
-__all__ = [
-    'User',
-    'Weight'
-]
+__all__ = ["User", "Weight", "Measurement"]
