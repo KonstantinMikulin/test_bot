be8cee49cb9dffb1423b5e88de4d62a13204d988
konstantin_mikulin <mikulin.konstantin@gmail.com>
2024-11-08 16:15:30 +0300

    Add MeasureBicep model for db

--- bot/db/models/__init__.py
+++ bot/db/models/__init__.py
@@ -2,5 +2,6 @@ from .user import User
 from .user_weights import Weight
 from .user_measure_tail import MeasureTail
 from .user_measure_hips import MeasureHips
+from .user_measure_bicep import MeasureBicep
 
-__all__ = ["User", "Weight", "MeasureTail", "MeasureHips"]
+__all__ = ["User", "Weight", "MeasureTail", "MeasureHips", "MeasureBicep"]
